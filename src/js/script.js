ff787a9c0d41cb6fb8fde6cb6bac79f9bf5d2ccc
Lucas Maieski <lucasgrigol@gmail.com>
2022-10-05 14:18:44 -0300

    final adjustments

--- src/js/script.js
+++ src/js/script.js
@@ -2,9 +2,18 @@ function menuShow() {
     let menuMobile = document.querySelector('.menu');
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "src/images/xmark-solid.svg";
+        document.querySelector('.icon').src = "src/images/close.png";
     } else {
         menuMobile.classList.add('open');
-        document.querySelector('.icon').src = "src/images/bars-solid.svg";
+        document.querySelector('.icon').src = "src/images/menu.png";
+    }
+}
+
+function changedSize() {
+    let menuMobile = document.querySelector('.menu');
+    if (window.innerWidth <= 630) {
+        menuMobile.classList.add('open');
+    } else {
+        menuMobile.classList.remove('open');
     }
 }
